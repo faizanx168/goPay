aecb75918b7052aa3dde2b9d17e9ef9f277d24f3
Muhammad Faizan Abbasi <faizanx168@gmail.com>
2024-06-19 17:13:00 -0400

    fixed envs

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
+
+module.exports = nextConfig;
